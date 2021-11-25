bbfec06a43710b650bbb4633c7a0bc5f3dd8d5fb
Philipp Tessenow <philipp@tessenow.org>
2021-11-25 18:46:00 +0100

    no dynamic angular

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -84,7 +84,8 @@ fi
 if [ "$1" == "run-rspec-features" ]; then
 	shift
 	execute "cd frontend; npm install ; cd -"
-	execute "npm run serve &"
+	execute "bundle exec rake assets:precompile"
+	execute "cp -rp config/frontend_assets.manifest.json public/assets/frontend_assets.manifest.json"
 
 	if ! execute "time bundle exec rspec --pattern '../plugin/spec/features/**/*_spec.rb'" ; then
 		execute "cat tmp/parallel_summary.log"
